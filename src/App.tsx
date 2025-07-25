72f4f59b8c9c29cc3491d611d55e53e03a973dd1
Ellie-Yeah <elisabeteff.69@gmail.com>
2025-07-25 22:16:59 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,6 @@ import D20Dice from './components/D20Dice'
   return (
     <>
     <D20Dice />
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="min-h-screen flex justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600 underline">
-          Tailwind está a funcionar!
-        </h1>
-      </div> */}
     </>
   )
 }
